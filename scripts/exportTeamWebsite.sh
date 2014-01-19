8dcf7bfd7134b6f002e49b81e7a4d8eb91d0335c
Travis Jefferson <jefferst@purdue.edu>
2014-01-19 17:22:14 -0500

    minor webapp updates, updated notebook and export script

--- scripts/exportTeamWebsite.sh
+++ scripts/exportTeamWebsite.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-cp -R /home/shay/a/477grp2/ECE477/src/website/* /home/shay/a/477grp2/Website
+scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
--- src/webapp/polls/admin.py
+++ src/webapp/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from polls.models import Poll
 # Register your models here.
+class PollAdmin(admin.ModelAdmin):
+    fieldsets = [ (None, {'fields':['question']}),
+                  ('Date info', {'fields':['pub_date'], 'classes':['collapse']}),
+                   ]
+
+admin.site.register(Poll, PollAdmin)
