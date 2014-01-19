6f85045cb3ee99cf98d13d60fcf3cfa43d210efa
Travis Jefferson <jefferst@purdue.edu>
2014-01-19 17:30:47 -0500

    changed export script to check for correct directory

--- scripts/exportTeamWebsite.sh
+++ scripts/exportTeamWebsite.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
+if [ "${PWD##*/}" = "scripts" ]
+then
+    scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
+else
+    echo "cd to the scripts directory before invoking this script"
+fi
