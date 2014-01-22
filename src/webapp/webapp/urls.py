04f4a2dee2194273ea9be23fc4097af6db05cf5d
Travis Jefferson <jefferst@purdue.edu>
2014-01-22 14:59:30 -0500

    started some views, added a gitignore, removed .pyc files

--- src/webapp/webapp/urls.py
+++ src/webapp/webapp/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'webapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+                       url(r'^polls/$', 'polls.views.index'),
+                       url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+                       url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+                       url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+                       url(r'^admin/', include(admin.site.urls)),
 )
