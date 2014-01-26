a6f11332d5637e89204d5b87bf59d87f35b0c8f7
Travis Jefferson <jefferst@purdue.edu>
2014-01-26 14:18:47 -0500

    finished step 5

--- src/webapp/polls/views.py
+++ src/webapp/polls/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from polls.models import Poll, Choice
-<<<<<<< HEAD
-from django.core.urlresolvers import reverse
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-=======
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
->>>>>>> 2cf66cf9fa7a8995f645d7ee1392e3e71ace7558
+from django.utils import timezone
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -15,29 +11,25 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_poll_list'
     def get_queryset(self):
-        return Poll.objects.order_by('-pub_date')[:5]
+        return Poll.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Poll
     template_name = 'polls/results.html'
+    def get_queryset(self):
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 def vote(request, poll_id):
     p = get_object_or_404(Poll, pk=poll_id)
     try:
         selected_choice = p.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-<<<<<<< HEAD
-        # Redisplay the poll voting form
-        return render(request, 'polls/detail.html', 
-                      {'poll':p, 'error_message' : "You didn't select a choice.", })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-=======
         # Redisplay the poll voting form.
         return render(request, 'polls/detail.html', {
             'poll': p,
@@ -49,5 +41,4 @@ def vote(request, poll_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
->>>>>>> 2cf66cf9fa7a8995f645d7ee1392e3e71ace7558
         return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
