10f7170ea794c447a5f4d2fd2791a30bb20f083f
travis <jefferst@purdue.edu>
2014-03-05 11:33:36 -0500

    changed website export script, should now only copy over newer files

--- scripts/exportTeamWebsite.sh
+++ scripts/exportTeamWebsite.sh
@@ -2,7 +2,7 @@
 if [ "${PWD##*/}" = "scripts" ]
 then
     #scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
-    rsync -vau ../src/website 477grp2@shay.ecn.purdue.edu:~/Website
+    rsync -vau ../src/website/* 477grp2@shay.ecn.purdue.edu:~/Website
 else
     echo "cd to the scripts directory before invoking this script"
 fi
