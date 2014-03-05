b0476fba02a21be4a77978b4868c756ac68973e0
travis <jefferst@purdue.edu>
2014-03-05 11:29:50 -0500

    notebook

--- scripts/exportTeamWebsite.sh
+++ scripts/exportTeamWebsite.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if [ "${PWD##*/}" = "scripts" ]
 then
-    scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
+    #scp -r ../src/website/* 477grp2@shay.ecn.purdue.edu:Website
+    rsync -vau ../src/website 477grp2@shay.ecn.purdue.edu:~/Website
 else
     echo "cd to the scripts directory before invoking this script"
 fi
