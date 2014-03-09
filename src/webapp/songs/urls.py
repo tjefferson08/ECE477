f0c1ccbdcfea85ccdc2ad03ce8421dab63a77edf
travis <jefferst@purdue.edu>
2014-03-09 17:39:03 -0400

    implemented a barebones API, notebook, negative voting, and some tiny bits of beautification

--- src/webapp/songs/urls.py
+++ src/webapp/songs/urls.py
@@ -4,13 +4,18 @@ from songs import views
 urlpatterns = patterns('',
                        # ex: /songs/
                        url(r'^$', views.IndexView.as_view(), name='index'),
-                       # ex: /songs/5/
-                       url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+
                        # ex: /songs/5/results/
                        url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+
                        # ex: /songs/1234/vote
                        url(r'^(?P<song_id>\d+)/vote$', views.vote, name='vote'),
+
                        # ex: /songs/searchSong
                        url(r'^searchSong/$', views.searchSong, name='searchSong'),
+
+                       # ex: /songs/requestNext
+                       url(r'^requestNext/$', views.provideNextSong, name='requestNext'),
+
                        )
 
--- src/webapp/songs/views.py
+++ src/webapp/songs/views.py
@@ -6,46 +6,58 @@ from django.views import generic
 from django.utils import timezone
 import requests
 
-# Create your views here.
+# home/landing page - shows top ten and search box
 class IndexView(generic.ListView):
     model = Song
     template_name = 'songs/index.html'
     context_object_name = 'latest_song_list'
     def get_queryset(self):
-        return Song.objects.order_by('-title')[:5]
+        return Song.objects.order_by('-votes')[:10]
 
-class DetailView(generic.DetailView):
-    model = Song
-    template_name = 'songs/detail.html'
-    def get_queryset(self):
-        return Song.objects.order_by('title')
-
+# once we search for, find, and vote in a song, 
+# we land on the 'view results/details' page
 class ResultsView(generic.DetailView):
     model = Song
     template_name = 'songs/results.html'
     context_object_name = "song"
 
+# vote is called both from the search results vote button and the
+# +1 and -1 buttons on index page, redirects to results page for
+# desired-vote-song
 def vote(request,song_id):
-    print "song id ", song_id
-    print "post data:"
-    print request.POST['title']
-    print request.POST['artist']
-    print request.POST['album']
+
+    # we should get the song in the DB from the index page, so we
+    # won't be adding dupe records
     try:
-        p = Song.objects.get(pk=song_id)
+        song = Song.objects.get(pk=song_id)
+
+    # this exception should occur when we vote from the search results
+    # page, in which case we'll make a new DB record and give it one vote
     except Song.DoesNotExist:
-        p = Song(id_code=song_id,
+        song = Song(id_code=song_id,
                  title=request.POST['title'],
                  artist=request.POST['artist'],
                  album=request.POST['album'],
                  votes=0)
-    p.votes += 1;
-    p.save()
+
+    # are we voting up or down? assume up in case of forgotten POST data
+    if ('vote_up' in request.POST):
+        vote_up = request.POST['vote_up']
+    else:
+        vote_up = "False"
+        
+    if (vote_up == "True"):
+        song.votes += 1;
+    elif (song.votes > 0): # if voting down, ensure vote # will remain nonnegative
+        song.votes -= 1;
+
+    # else, do nothing (don't vote down)
+    song.save()
 
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
-    # user hits the Back button.
-    return HttpResponseRedirect(reverse('songs:results', args=(p.id_code,)))
+    # user hits the Back button. We redirect to the song-level results/info page
+    return HttpResponseRedirect(reverse('songs:results', args=(song.id_code,)))
 
 # we call this function by a button press on index.html
 def searchSong(request):
@@ -56,7 +68,8 @@ def searchSong(request):
         search_string = "Never gonna give you up"
     print search_string
 
-    # api concatenation
+    # api concatenation and call, response stored in r, json passed to
+    # template in context var
     tinysong_api_key = "da4d9bf5ee352d1f99eef3b73a8601cf"
     tinysong_url = "http://www.tinysong.com/s/"
     api_params = { 'key':tinysong_api_key, 'format':'json', 'limit':10 }
@@ -64,3 +77,7 @@ def searchSong(request):
     context = {'tiny_json':r.json()}
     return render(request, 'songs/searchResults.html', context)
 
+# get a req for next song(s), reply with top 5 song IDs
+def provideNextSong(request): 
+    return HttpResponse(Song.objects.order_by('-votes')[0], content_type = "text/plain")
+    
