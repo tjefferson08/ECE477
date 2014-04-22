10a160d8dad64fdfc97a06844529d011ed9bc179
Matthew McMunigle <mmcmunig@purdue.edu>
2014-04-22 18:01:16 +0000

    fixed top-level url, it now redirects properly

--- src/webapp/webapp/urls.py
+++ src/webapp/webapp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
-
+from django.http import HttpResponseRedirect
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
                        url(r'^songs/', include('songs.urls', namespace="songs")),
+                       url(r'^$', lambda x: HttpResponseRedirect('/songs/')),
                        url(r'^admin/', include(admin.site.urls)),
                        )
